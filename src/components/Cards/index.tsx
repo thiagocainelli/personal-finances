a510a11e7543e22aeef94dd8cc418e3d25e76b7d
Thiago Cainelli <thiagocainelli@gmail.com>
2023-10-20 11:06:31 -0300

    Creating Cards and Desctiptions components

---
+++ src/components/Cards/index.tsx
@@ -0,0 +1,16 @@
+interface CardsType {
+    text: string,
+    icon: any,
+    value: number
+}
+
+export default function Cards ({text, icon, value}: CardsType) {
+    return (
+        <>
+            <div className="flex flex-col gap-5 grow min-w-[320px] rounded-md bg-white text-gray-900 py-2 px-3">
+                <p className="flex w-full justify-between px-5 items-center text-2xl">{text}{icon}</p>
+                <p className="text-2xl font-bold text-center">R$ {value}</p>
+            </div>
+        </>
+    )
+}
---
+++ src/components/Descriptions/index.tsx
@@ -0,0 +1,60 @@
+interface InputsTypes {
+    description: string,
+    setDescription: (ev:any) => void,
+    value: number,
+    setValue: (ev: any) => void,
+    clickButton: any
+}
+
+export default function Descriptions ({description, setDescription, value, setValue, clickButton}: InputsTypes) {  
+    return (
+        <>
+            <div className="flex items-center justify-between px-5 py-2 text-gray-900 bg-white">
+                <div>
+                    <p>Descrição</p>
+                    <input
+                        className="border border-gray-900 rounded-md outline-none px-2" 
+                        type="text"
+                        value={description}
+                        onChange={(ev) => setDescription(ev.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <p>Valor</p>
+                    <input
+                        className="border border-gray-900 rounded-md outline-none px-2" 
+                        type="number"
+                        value={value}
+                        onChange={(ev) => setValue(ev.target.value)}
+                    />
+                </div>
+
+                <div className="flex items-center gap-3">
+                    <div>
+                        <input
+                            type="radio"
+                            name="transition"
+                            id="depositMoney"
+                        />
+                        <label className="ms-1" htmlFor="depositMoney">Entrada</label>
+                    </div>
+
+                    <div>
+                        <input
+                            type="radio"
+                            name="transition"
+                            id="outMoney"
+                        />
+                        <label className="ms-1" htmlFor="outMoney">Saida</label>
+                    </div>
+                </div>
+
+                <div>
+                    <button className="bg-green-600 px-3 py-2 rounded-md text-white hover:bg-green-800 active:opacity-50" onClick={clickButton}>Adicionar</button>
+                </div>
+
+            </div>
+        </>
+    )
+}
